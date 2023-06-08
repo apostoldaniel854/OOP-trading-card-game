61ce8eb7a0d226330caf0f78248b2b7ec1d4c6b1
DESKTOP-7JEFSDS\Pc <apostoldaniel854@gmail.com>
2023-06-08 20:54:49 +0300

    test to see issue with CI

--- controller.cpp
+++ controller.cpp
@@ -1,6 +1,11 @@
 #include "headers/game.h"
 
 int main() {
+    std::string a = "aa";
+    std::cout << "Este o instanta umana?\n";
+    std::cin >> a;
+    if (a == "aa")
+        return 0;
     Game game;
 //    std::cout << game << "\n"; /// test required for hw 1
     game.run();
